1887d0ec21b7c0896a22a19f048d04d6481326db
Willy Aguirre <marti1125@gmail.com>
2018-01-08 22:10:30 -0500

    add 3 examples

---
+++ JustJava/app/src/main/java/com/justjava/app/justjava/MainActivity.java
@@ -0,0 +1,45 @@
+package com.justjava.app.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    public void submitOrder(View view) {
+        int coffeePrice = 5;
+        displayPrice(quantity * coffeePrice);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
